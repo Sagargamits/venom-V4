d793bce451218eda6b9f4b323318c667395fddd1
Sagargamits <sagargmt2@gmail.com>
2024-07-27 23:33:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@
 BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("sagargamit")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7424479555")
 
 
 
